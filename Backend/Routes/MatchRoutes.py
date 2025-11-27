fefc0da767445862a73378b1bad19a81dddabde6
alexlopez7498 <130875480+alexlopez7498@users.noreply.github.com>
2025-11-26 17:25:14 -0800

    Created a routes folder to organize routes for each model

---
+++ Backend/Routes/MatchRoutes.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel
+from typing import Union
+from typing import List, Annotated
+from Models.team import Base
+from database import engine, get_db
+from sqlalchemy.orm import Session
+from Controllers.TeamController import importLeagueTable, readTeams, createTeam, TeamBase
+from Controllers.PlayerController import importPlayers, readAllPlayers, readPlayersPerTeam, createPlayer, PlayerBase
+from Controllers.MatchController import importMatches, readAllMatches, readMatchesPerTeam, createMatch, MatchBase
+
+router = APIRouter()
+
+#API call get request to get all matches in the database
+@router.get("/matches/", tags=["matches"])
+async def getAllMatches(db: Session = Depends(get_db)):
+    return readAllMatches(db)
+
+@router.post("/matches/import", tags=["matches"])
+async def importAllMatches(db: Session = Depends(get_db)):
+    return await importMatches("../WebScraper/schedules_2025_2026.csv", db)
+
+#API call get request to get all players from a specific team
+@router.get("/matches/team/{team_name}", tags=["matches"])
+async def getMatchesPerTeam(team_name: str, db: Session = Depends(get_db)):
+    return await readMatchesPerTeam(team_name, db)
+
+#API call post request to add a player to the database
+@router.post("/matches/", tags=["matches"])
+async def addMatch(match: MatchBase, db: Session = Depends(get_db)):
+    return await createMatch(match,db)
